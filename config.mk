727c14019a44fac80ab817605cfc4f5b6a27fa9e
Bailey Stoner <monokrome@monokro.me>
2018-11-26 18:00:06 -0800

    MacOS customization

--- config.mk
+++ config.mk
@@ -1,14 +1,10 @@
-# Customize below to fit your system
-
 PREFIX ?= /usr/local
 MANPREFIX = ${PREFIX}/share/man
-# specify your systems terminfo directory
-# leave empty to install into your home folder
-TERMINFO := ${DESTDIR}${PREFIX}/share/terminfo
+TERMINFO := /usr/share/terminfo
 
 INCS = -I.
-LIBS = -lc -lutil -lncursesw
+LIBS = -lc -lutil -lncurses
 CPPFLAGS = -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_XOPEN_SOURCE_EXTENDED
-CFLAGS += -std=c99 ${INCS} -DNDEBUG ${CPPFLAGS}
+CFLAGS += -std=c99 ${INCS} -DNDEBUG ${CPPFLAGS} -D_DARWIN_C_SOURCE
 
 CC ?= cc
